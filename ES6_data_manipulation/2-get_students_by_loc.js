7f266e14c47219632c8214d0b9b57493322da699
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2023-03-29 00:27:29 -0500

    Update 2-get_students_by_loc.js

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(listSts, city) {
   if (Array.isArray(listSts)) {
-    return listSts.filter((listSts) =>
-                          listSts.location === city)}
+    return listSts.filter((listSts)=>
+      listSts.location === city) };
   return [];
 }
