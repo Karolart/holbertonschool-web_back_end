afedda9762dfc094b15ef3982044699b1bb9d649
Karolart <karoljuzz1610@gmail.com>
2023-03-29 00:16:14 -0500

    file 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+function updateStudentGradeByCity(listStudents, city, newGrades) {
+  return listStudents
+    .filter((listStudents) => listStudents.location === city)
+    .map((listStudents) => {
+      const newGrade = newGrades.find((grade) => grade.studentId === listStudents.id);
+        return {
+        ...listStudents,
+        grade: newGrade ? newGrade.grade : 'N/A',
+        };
+      });
+  }
+  
+  export default updateStudentGradeByCity;
+  
