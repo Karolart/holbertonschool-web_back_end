fcd9feb71833b55bda7cafc5a2c78db6c9d74f80
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2023-03-29 00:25:12 -0500

    Update 4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,12 +3,11 @@ function updateStudentGradeByCity(listStudents, city, newGrades) {
     .filter((listStudents) => listStudents.location === city)
     .map((listStudents) => {
       const newGrade = newGrades.find((grade) => grade.studentId === listStudents.id);
-        return {
+      return {
         ...listStudents,
         grade: newGrade ? newGrade.grade : 'N/A',
-        };
-      });
-  }
-  
-  export default updateStudentGradeByCity;
-  
+      };
+    });
+}
+
+export default updateStudentGradeByCity;
