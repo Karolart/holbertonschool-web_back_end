da3acf96aca19d1499bf68fa583ad60047a7f8f5
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2023-04-11 16:38:52 -0500

    Update 9-try.js

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -2,14 +2,14 @@ export default function guardrail(mathFunction) {
   const queue = [];
   let value;
 
-try {
-  value = mathFunction();
+  try {
+      value = mathFunction();
   } catch (err) {
   value = err.toString();
   }
-  
+
   queue.push(value);
   queue.push('Guardrail was processed');
- 
+
   return queue;
 }
