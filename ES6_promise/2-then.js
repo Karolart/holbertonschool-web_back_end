c66d6e82080238eb692411913bf9dfc5bc84e134
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2023-04-11 15:39:26 -0500

    Update 2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
-       status: 200,
-        body: 'success',
-      }))
-      .catch(() => Error())
-      .finally(() => console.warn('Got a response from the API'));
-  }
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
+}
   
-  export default handleResponseFromAPI;
+export default handleResponseFromAPI;
