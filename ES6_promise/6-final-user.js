24a7cfc7b5487861dd25fd1da38a8f465ef3697b
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2023-04-11 16:31:09 -0500

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
-import signUpUser from "./4-user-promise"
-import uploadPhoto from "./5-photo-reject"
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = {
     status: 'pending ',
-    };
+  };
   const promise2 = {
     status: 'pending ',
   };
-  
+
   try {
     const signup = await signUpUser(firstName, lastName);
     promise1.status = 'fulfilled';
@@ -16,7 +16,7 @@ async function handleProfileSignup(firstName, lastName, fileName) {
   } catch (err) {
     promise1.status = 'rejected';
     promise1.value = err.toString();
-    }
+  }
  
   try {
     const upload = await uploadPhoto(fileName);
@@ -26,8 +26,7 @@ async function handleProfileSignup(firstName, lastName, fileName) {
     promise2.status = 'rejected';
     promise2.value = err.toString();
   }
-  
+
   return [promise1, promise2];
 }
-  
 export default handleProfileSignup;
